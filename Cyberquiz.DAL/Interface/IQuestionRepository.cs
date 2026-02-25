f47ff27199e566113cfe50f71640fc97f798cfdc
roerstrand <129670382+roerstrand@users.noreply.github.com>
2026-02-25 11:23:56 +0100

    Add project files.
    Add DAL models, DbContexts, and update project references
    
    Introduced AppDbContext and AuthDbContext for EF Core integration. Added data models for categories, questions, answers, and user results. Updated project references and connection string to use CyberquizDB. Added repository stubs. Minor/incomplete change in Home.razor.

---
+++ Cyberquiz.DAL/Interface/IQuestionRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cyberquiz.DAL.Interface
+{
+    public class IUserRepository
+    {
+
+    }
+}
---
+++ Cyberquiz.DAL/Models/SubCategoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cyberquiz.DAL.Models
+{
+    public class SubCategoryModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string CategoryModelId { get; set; }
+
+        public int QuestionModelId { get; set; }
+
+    }
+}
