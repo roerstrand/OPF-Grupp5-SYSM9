de1106f5bc8c5b5f06a018743d973a5ff79d8a25
Ditt Namn <linda-maria.m@hotmail.com>
2026-03-05 15:38:53 +0100

    SubmitAnswerAsync behäver fångas upp i Repo.

--- Cyberquiz.BLL/Services/QuestionService.cs
+++ Cyberquiz.BLL/Services/QuestionService.cs
@@ -13,24 +13,37 @@ namespace Cyberquiz.BLL.Services
         {
             _questionRepo = questionRepo;
         }
-        public async Task<QuestionDto?> GetByIdAsync(int id)
-        {
-            var question = await _questionRepo.GetByIdAsync(id);
-            return question == null ? null : MapToQuestionDto(question);
-        }
 
+        // Metod för ENDPOINT "subcategory/{subCategoryId:int}/questions" som hämtar alla frågor inom en underkategori
         public async Task<IEnumerable<QuestionDto>> GetBySubCategoryAsync(int subCategoryId)
         {
             var questions = await _questionRepo.GetBySubCategoryAsync(subCategoryId);
             return questions.Select(MapToQuestionDto);
         }
 
-        // Metod för ENDPOINT "subcategory/{subCategoryId:int}/next"
-        // Metoden hämtar nästa fråga för en användare inom en specifik subkategori, baserat på användarens tidigare svar och framsteg
+        // Metod för ENDPOINT "questions/{id:int}" som hämtar en enskild fråga med svarsalternativ
+        public async Task<QuestionDto?> GetByIdAsync(int id)
+        {
+            var question = await _questionRepo.GetByIdAsync(id);
+            return question == null ? null : MapToQuestionDto(question);
+        }
+
+        // Metod för ENDPOINT "subcategory/{subCategoryId:int}/next" som hämtar nästa fråga inom underkategori utifrån användarens tidigare svar och framsteg
         public async Task<QuestionDto?> GetNextQuestionAsync(string userName, int subCategoryId)
         {
-            var question = await _questionRepo.GetNextQuestionAsync(userName, subCategoryId);
+            var question = await _questionRepo.GetNextQuestionAsync(userName, subCategoryId); // Behöver fångas upp i Repo!
             return question == null ? null : MapToQuestionDto(question);
+        }
+
+        // Metod för ENDPOINT "answer" som tar emot användarens svar och uppdaterar framsteg
+        public async Task<SubmitResponseDto> SubmitAnswerAsync(string userName, SubmitAnswerRequestDto request)
+        {
+            var result = await _questionRepo.SubmitAnswerAsync(userName, request); // Behöver fångas upp i Repo!
+            return new SubmitResponseDto
+            {
+                IsCorrect = result.IsCorrect,
+                CorrectAnswerId = result.CorrectAnswerId
+            };
         }
 
         // Mapping-metod från Model till Dto
