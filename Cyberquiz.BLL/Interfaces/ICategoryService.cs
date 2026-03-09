9267ee7a4899d64a070c5cfddb0bab8fe411ddaa
Ditt Namn <linda-maria.m@hotmail.com>
2026-03-09 10:47:00 +0100

    Lagt till ingående parameter userName i Services/IServices (samt kommentrarer i motsvarande repon) för att appen ska veta vem som är inloggad.

--- Cyberquiz.BLL/Interfaces/ICategoryService.cs
+++ Cyberquiz.BLL/Interfaces/ICategoryService.cs
@@ -4,10 +4,10 @@ namespace Cyberquiz.BLL.Interfaces
 {
     public interface ICategoryService
     {
-        Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync(); //Behöver ev. string username som parameter? Men det sköter väl Identity?
-        Task<CategoryDto?> GetCategoryByIdAsync(int id);
+        Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync(string userName); 
+        Task<CategoryDto?> GetCategoryByIdAsync(string userName, int id);
 
-        Task<IEnumerable<SubCategoryDto>> GetAllSubCategoriesAsync();
-        Task<SubCategoryDto?> GetSubCategoryByIdAsync(int id); //Behöver ev. string username och int id som parameter? Men det sköter väl Identity?
+        Task<IEnumerable<SubCategoryDto>> GetAllSubCategoriesAsync(string userName);
+        Task<SubCategoryDto?> GetSubCategoryByIdAsync(string userName, int id); 
     }
 }
--- Cyberquiz.BLL/Interfaces/IQuestionService.cs
+++ Cyberquiz.BLL/Interfaces/IQuestionService.cs
@@ -5,8 +5,8 @@ namespace Cyberquiz.BLL.Interfaces
     public interface IQuestionService
     {
         Task<QuestionDto?> GetByIdAsync(int id);
-        Task<IEnumerable<QuestionDto>> GetBySubCategoryAsync(int subCategoryId);
-        Task<QuestionDto?> GetNextQuestionAsync(string userName, int subCategoryId); // hämtar nästa fråga som användaren inte har svarat på i den subkategorin
-        Task<SubmitResponseDto> SubmitAnswerAsync(string userName, SubmitAnswerRequestDto request); //tar emot användarens svar, sparar det och returnerar om det var rätt eller fel
+        //Task<IEnumerable<QuestionDto>> GetBySubCategoryAsync(int subCategoryId, string userName); // METOD ANVÄNDS INTE I NUVARANDE VERSION
+        Task<QuestionDto?> GetNextQuestionAsync(int subCategoryId, string userName); // hämtar nästa fråga som användaren inte har svarat på i den subkategorin
+        Task<SubmitResponseDto> SubmitAnswerAsync(SubmitAnswerRequestDto request, string userName); //tar emot användarens svar, sparar det och returnerar om det var rätt eller fel
     }
 }
