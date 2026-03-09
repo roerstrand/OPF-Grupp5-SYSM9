bd6140954f196f849558abb734c5116b73f52bb7
Ditt Namn <linda-maria.m@hotmail.com>
2026-03-09 10:09:00 +0100

    Utökat kommentarer i CatServ och ICatServ angående hur Identity opererar genom appen.

--- Cyberquiz.BLL/Interfaces/ICategoryService.cs
+++ Cyberquiz.BLL/Interfaces/ICategoryService.cs
@@ -4,10 +4,10 @@ namespace Cyberquiz.BLL.Interfaces
 {
     public interface ICategoryService
     {
-        Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync(); //Behöver ev. string username som paramater
+        Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync(); //Behöver ev. string username som parameter? Men det sköter väl Identity?
         Task<CategoryDto?> GetCategoryByIdAsync(int id);
 
         Task<IEnumerable<SubCategoryDto>> GetAllSubCategoriesAsync();
-        Task<SubCategoryDto?> GetSubCategoryByIdAsync(int id); //Behöver ev. string username och int id som paramater
+        Task<SubCategoryDto?> GetSubCategoryByIdAsync(int id); //Behöver ev. string username och int id som parameter? Men det sköter väl Identity?
     }
 }
