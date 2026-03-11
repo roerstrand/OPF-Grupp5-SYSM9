29fbc5673f6856d46a7695fecb83ed8d4723bbf0
Ditt Namn <linda-maria.m@hotmail.com>
2026-03-11 23:11:26 +0100

    Lagt till AnswerValidationDto i Q-Controller

---
+++ Cyberquiz.Shared/DTOs/AnswerValidationDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cyberquiz.Shared.DTOs
+{
+    public class AnswerValidationDto
+    {
+        public bool IsCorrect { get; set; }
+        public int CorrectAnswerOptionId { get; set; }
+
+    }
+}
