ec7396c37b3ad2a7baeeaa5714d19de504494be7
Ditt Namn <linda-maria.m@hotmail.com>
2026-03-11 23:40:16 +0100

    Skapat filer för AI-coach: * AiService * IAiService * AiController

---
+++ Cyberquiz.API/Controllers/AiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cyberquiz.API.Controllers
+{
+    public class AiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
